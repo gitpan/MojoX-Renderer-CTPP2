d9f205834bd39fdc4c803509a596ef41555e8dc6
Victor M Elfimov <victor@sols.ru>
2009-12-11 23:56:23 +0000

    Import of SHADOVV/MojoX-Renderer-CTPP2-0.02 from CPAN.
    gitpan-cpan-distribution: MojoX-Renderer-CTPP2
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHADOVV/MojoX-Renderer-CTPP2-0.02.tar.gz
    gitpan-cpan-author:       SHADOVV
    gitpan-cpan-maturity:     released

--- lib/MojoX/Renderer/CTPP2.pm
+++ lib/MojoX/Renderer/CTPP2.pm
@@ -5,51 +5,85 @@ use warnings;
 
 use base 'Mojo::Base';
 
-use HTML::CTPP2 ();
-use Carp        ();
-
 use File::Spec ();
 use File::Path qw/make_path/;
 
-our $VERSION = '0.01';
+use Carp        ();
+use HTML::CTPP2 ();
 
-__PACKAGE__->attr('ctpp2', chained => 1);
+our $VERSION = '0.02';
+
+__PACKAGE__->attr('ctpp2');
+
+my %ctpp2_allow_params = map { $_, 1 }
+  qw/arg_stack_size code_stack_size steps_limit max_functions source_charset destination_charset/;
 
 sub build {
     my $self = shift->SUPER::new(@_);
-    my %args = @_;
 
-    my %config = (%{$args{template_options} || {}});
-
-    Carp::carp "Not defined 'mojo' param. Relative path for compiled templates ignored. Use path - "
-      . File::Spec->catdir(File::Spec->tmpdir, $self->{COMPILE_DIR})
-      if !$self->{mojo}
-          && ($self->{COMPILE_DIR} && ! File::Spec->file_name_is_absolute($self->{COMPILE_DIR}));
-
-    $self->ctpp2(HTML::CTPP2->new(%config))
-      or Carp::croak 'Could not initialize CTPP2 object';
+    $self->_init_ctpp2(@_);
 
     return sub { $self->_render(@_) };
 }
 
+sub _init_ctpp2 {
+    my $self = shift;
+    my %args = @_;
+
+    my $mojo = $args{mojo};
+    my %config = %{delete $args{template_options} || {}};
+
+    $self->{COMPILE_EXT} ||= '.ctpp2c';
+
+    $self->{CACHE_ENABLE} = 1 if not exists $self->{CACHE_ENABLE};
+
+    unless (exists $self->{COMPILE_DIR}) {
+        $self->{COMPILE_DIR} = $mojo ? $mojo->home->rel_dir('tmp/ctpp2') : File::Spec->tmpdir;
+    }
+
+    for (keys %config) {
+        exists $ctpp2_allow_params{$_} or delete $config{$_};
+    }
+
+    $self->ctpp2(HTML::CTPP2->new(%config)) or Carp::croak 'Could not initialize CTPP2 object';
+
+    if (!(ref $args{INCLUDE_PATH} eq 'ARRAY')) {
+        my @include_path = _coerce_paths($args{INCLUDE_PATH}, $args{DELIMITER});
+        if (!@include_path) {
+            @include_path = $mojo ? $mojo->home->rel_dir('templates') : ();
+        }
+        $args{INCLUDE_PATH} = \@include_path;
+    }
+
+    $self->ctpp2->include_dirs(\@{$args{INCLUDE_PATH}});
+
+    return $self;
+}
+
+sub _coerce_paths {
+    my ($paths, $dlim) = @_;
+
+    return () if (!$paths);
+    return @{$paths} if (ref $paths eq 'ARRAY');
+
+    $dlim = ($^O eq 'MSWin32') ? ':(?!\\/)' : ':' unless (defined $dlim);
+
+    return split(/$dlim/, $paths);
+}
+
 sub _render {
-    my ($self, $renderer, $c, $output) = @_;
+    my ($self, $renderer, $c, $output, $options) = @_;
 
-    my $template_path = $c->stash->{template_path};
+    my $ctpp2         = $self->ctpp2;
+    my $template_path = $c->stash->{'template_path'} || $renderer->template_path($options);
+    my $bytecode      = $self->_get_bytecode($renderer->root, $template_path);
 
-    my $bytecode =
-      ($self->{COMPILE_DIR} || $self->{COMPILE_EXT})
-      ? $self->compile_bytecode($template_path, $renderer)
-      : $self->ctpp2->parse_template($template_path);
+    $ctpp2->param({%{$c->stash}, base => $c->tx->req->url->base->to_string});
 
-    $self->ctpp2->param($c->stash);
-
-    $$output = $self->ctpp2->output($bytecode);
-
-    unless ($$output = $self->ctpp2->output($bytecode)) {
+    unless ($$output = $ctpp2->output($bytecode)) {
         Carp::carp 'Template error: ['
-          . $self->ctpp2->get_last_error->{'template_name'} . '] - '
-          . $self->ctpp2->get_last_error->{'error_str'};
+          . $ctpp2->get_last_error->{'template_name'} . '] - '
+          . $ctpp2->get_last_error->{'error_str'};
         return 0;
     }
     else {
@@ -57,53 +91,46 @@ sub _render {
     }
 }
 
-sub compile_bytecode {
-    my ($self, $template_path, $renderer) = @_;
+sub _get_bytecode {
+    my ($self, $templates_rootdir, $template) = @_;
 
-    my $compile_dir;
-    if ($self->{mojo}) {
-        $compile_dir =
-            $self->{COMPILE_DIR}
-          ? File::Spec->file_name_is_absolute($self->{COMPILE_DIR})
-              ? $self->{COMPILE_DIR}
-              : $self->{mojo}->home->rel_dir($self->{COMPILE_DIR})
-          : $self->{mojo}->home->rel_dir('tmp/ctpp2');
-    }
-    else {
-        $compile_dir =
-            $self->{COMPILE_DIR}
-          ? File::Spec->file_name_is_absolute($self->{COMPILE_DIR})
-              ? $self->{COMPILE_DIR}
-              : File::Spec->catdir(File::Spec->tmpdir, $self->{COMPILE_DIR})
-          : File::Spec->catdir(File::Spec->tmpdir, 'ctpp2');
-    }
+    my $ctpp2 = $self->ctpp2;
 
-    my $template = File::Spec->abs2rel($template_path, $renderer->root);
-    $template =~ s{\.[^\.]+$}{};
-    $template .= $self->{COMPILE_EXT} || '.ctpp2c';
-    $template = File::Spec->catfile($compile_dir, $template);
+    if ($self->{CACHE_ENABLE}) {
+        my $mojo            = $self->{mojo};
+        my $compile_rootdir = $self->{COMPILE_DIR};
 
-    my $bytecode;
-    if (-e $template) {
-        if ((stat($template))[9] < (stat($template_path))[9]) {
-            $bytecode = $self->ctpp2->parse_template($template_path);
-            $bytecode->save($template);
+        my $template_relpath = File::Spec->abs2rel($template, $templates_rootdir);
+        $template_relpath  =~ s{\.[^\.]+$}{};
+        $template_relpath .= $self->{COMPILE_EXT};
+
+        my $ctemplate = File::Spec->catfile($compile_rootdir, $template_relpath);
+
+        my $bytecode;
+        if (-e $ctemplate) {
+            if ((stat($ctemplate))[9] < (stat($template))[9]) {
+                $bytecode = $ctpp2->parse_template($template);
+                $bytecode->save($ctemplate) if $bytecode;
+            }
+            else {
+                return $ctpp2->load_bytecode($ctemplate);
+            }
         }
         else {
-            $bytecode = $self->ctpp2->load_bytecode($template);
+            $bytecode = $ctpp2->parse_template($template);
+
+            my $save_path = $ctemplate;
+            $save_path =~ s{/[^/]+$}{};
+
+            make_path($save_path) if !-d $save_path;
+
+            $bytecode->save($ctemplate) if $bytecode;
         }
+        return $bytecode;
     }
     else {
-        $bytecode = $self->ctpp2->parse_template($template_path);
-
-        my $save_path = $template;
-        $save_path =~ s{/[^/]+$}{};
-
-        make_path($save_path) if !-d $save_path;
-
-        $bytecode->save($template);
+        $ctpp2->parse_template($template);
     }
-    return $bytecode;
 }
 
 1;
@@ -120,31 +147,40 @@ MojoX::Renderer::CTPP2 - CTPP2 renderer for Mojo
 
 Add the handler:
 
-    use MojoX::Renderer::CTPP2;
+  use MojoX::Renderer::CTPP2;
 
-    sub startup {
-       ...
+  sub startup {
+     ...
 
-       my $ctpp2 = MojoX::Renderer::CTPP2->build(
-            mojo        => $self,
-            COMPILE_DIR => '/tmp/ctpp',
-            COMPILE_EXT => '.ctpp2'
-            template_options =>
-             { arg_stack_size => 1024,
-               arg_stack_size => 2048
-             }
-       );
+    my $ctpp2 = MojoX::Renderer::CTPP2->build(
+      mojo         => $self,
 
-       $self->renderer->add_handler( ctpp2 => $ctpp2 );
+      INCLUDE_PATH => '/tmp;/tmp/project1',
+      DELIMITER    => ';',
 
-       ...
-    }
+      CACHE_ENABLE => 0,
+      COMPILE_DIR  => '/tmp/ctpp',
+      COMPILE_EXT  => '.ctp2',
+
+      template_options =>
+        { arg_stack_size => 1024,
+          steps_limit    => 1024*1024
+        }
+      );
+
+      $self->renderer->add_handler( ctpp2 => $ctpp2 );
+
+     ...
+  }
 
 And then in the handler call render which will call the
 MojoX::Renderer::CTPP2 renderer.
 
-   $c->render(foo => 653, bar => [qw(abc 7583 def)]);
+  $c->render(templatename, format => 'htm', handler => 'ctpp2');
 
+Template parameter are taken from $c->stash :
+
+  $c->stash(users => [John, Peter, Ann]);
 
 =head1 METHODS
 
@@ -158,95 +194,71 @@ Supported parameters are:
 
 =item mojo
 
-C<new> currently requires a I<mojo> parameter pointing to the base class Mojo-object. (Need for initial path to compiled templates.)
+C<build> currently uses a C<mojo> parameter pointing to the base class Mojo-object.
+
+=item INCLUDE_PATH
+
+The C<INCLUDE_PATH> is used to specify one or more directories in which
+template files are located.  When a template is requested that isn't
+defined locally as a C<BLOCK>, each of the C<INCLUDE_PATH> directories is
+searched in turn to locate the template file.  Multiple directories
+can be specified as a reference to a list or as a single string where
+each directory is delimited by 'C<:>'.
+
+  INCLUDE_PATH => '/project1/templates/1'
+
+  INCLUDE_PATH => '/myapp/path1:/myapp/path2:path3'
+
+  INCLUDE_PATH => [
+    '/project1/templates/1',
+    '/myapp/path2'
+  ]
+
+On Win32 systems, a little extra magic is invoked, ignoring delimiters
+that have 'C<:>' followed by a 'C</>' or 'C<\>'.  This avoids confusion when using
+directory names like 'C<C:\Blah Blah>'.
+
+=item DELIMITER
+
+Used to provide an alternative delimiter character sequence for
+separating paths specified in the C<INCLUDE_PATH>.  The default
+value for C<DELIMITER> is 'C<:>'.
+
+  DELIMITER => ';'
+
+On Win32 systems, the default delimiter is a little more intelligent,
+splitting paths only on 'C<:>' characters that aren't followed by a 'C</>'.
+This means that the following should work as planned, splitting the
+C<INCLUDE_PATH> into 2 separate directories, C<C:/foo> and C<C:/bar>.
+
+  # on Win32 only
+  INCLUDE_PATH => 'C:/Foo:C:/Bar'
+
+However, if you're using Win32 then it's recommended that you
+explicitly set the C<DELIMITER> character to something else (e.g. 'C<;>')
+rather than rely on this subtle magic.
+
+=item CACHE_ENABLE
+
+The C<CACHE_ENABLE> can be set 0 to disable templates caching. Default - caching enable.
 
 =item COMPILE_DIR
 
-Root of directory in which compiled template files should be written.
+The C<COMPILE_DIR> option is used to specify an alternate directory root
+under which compiled template files should be saved.
 
-I<If not defined B<'COMPILE_DIR'> or B<'COMPILE_EXT'> params - template don't compile and save.>
+  COMPILE_DIR => '/tmp/ctpp'
 
 =item COMPILE_EXT
 
-Filename extension for compiled template files (default - .ctpp2c).
+The C<COMPILE_EXT> option may be provided to specify a filename extension for
+compiled template files. It is undefined by default used extension '.ctpp2c' .
+
+  COMPILE_EXT => '.ccc'
 
 =item template_options
 
-A hash reference of options that are passed to CTPP2->new().
-
-=back
-
-=head3 About path for compiled templates:
-
-=over
-
-=item *
-
-If B<mojo> and B<COMPILE_DIR> are defined:
-
-=over
-
-=item *
-
-B<COMPILE_DIR> is absolute path - use this absolute path (ex.: mojo project root path - '/mojo', COMPILE_DIR - '/c_tmpl', path for compiled templates - '/c_tmpl')
-
-=item *
-
-B<COMPILE_DIR> is relative path - use relative path into B<mojo> root directory (ex.: mojo project root path - '/mojo', COMPILE_DIR - 'tmp/c_tmpl', path for compiled templates - '/mojo/tmp/c_tmpl')
-
-=back
-
-=back
-
-=over
-
-=item *
-
-If B<mojo> defined and B<COMPILE_DIR> not defined:
-
-=over
-
-=item *
-
-use relative path 'tmp/ctpp2' into B<mojo> root directory (ex.: mojo project root path - '/mojo', path for compiled templates - '/mojo/tmp/ctpp2')
-
-=back
-
-=back
-
-=over
-
-=item *
-
-If B<mojo> not defined and B<COMPILE_DIR> are defined:
-
-=over
-
-=item *
-
-B<COMPILE_DIR> is absolute path - use this absolute path (COMPILE_DIR - '/c_tmpl', path for compiled templates - '/c_tmpl')
-
-=item *
-
-B<COMPILE_DIR> is relative path - use relative path into C<File::Spec-E<gt>tmpdir> function directory (ex.: system temporary path - '/tmp', COMPILE_DIR - 'c_tmpl', path for compiled templates - '/tmp/c_tmpl')
-
-=back
-
-=back
-
-=over
-
-=item *
-
-Both parameters B<mojo> and B<COMPILE_DIR> not defined:
-
-=over
-
-=item *
-
-use relative path 'ctpp2' into C<File::Spec-E<gt>tmpdir> function directory (ex.: system temporary path - '/tmp', path for compiled templates - '/tmp/ctpp2')
-
-=back
+A hash reference of options that are passed to HTML::CTPP2->new(). See also L<HTML::CTPP2>
 
 =back
 
@@ -257,14 +269,14 @@ Victor M Elfimov, (victor@sols.ru)
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-mojox-renderer-ctpp2 at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MojoX-Renderer-CTPP2>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MojoX-Renderer-CTPP2>.
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
-    perldoc MojoX::Renderer::CTPP2
+  perldoc MojoX::Renderer::CTPP2
 
 You can also look for information at:
 
@@ -286,7 +298,7 @@ L<http://search.cpan.org/dist/MojoX-Renderer-CTPP2/>
 
 =head1 SEE ALSO
 
-HTML::CTPP2(3)
+HTML::CTPP2(3) Mojo(3) MojoX::Renderer(3)
 
 =head1 COPYRIGHT & LICENSE
 
@@ -296,3 +308,4 @@ This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
+
